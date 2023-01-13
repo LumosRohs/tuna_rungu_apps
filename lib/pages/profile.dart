a559a088350059896427a9d336f1a75661bcd87e
isalzufari <isalzufari@gmail.com>
2023-01-13 17:34:41 +0700

    changeName

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfilePage extends StatefulWidget {
   final String displayName;
@@ -13,6 +14,34 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final changeDisplayName = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+      if (user != null) {
+        print(user.displayName);
+      }
+    });
+  }
+
+  void changeDisplayNameFirebase(displayName) {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+      if (user != null) {
+        await user.updateDisplayName(displayName);
+      }
+    });
+  }
+
+  void changePhotoUrlFirebase(url) {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+      if (user != null) {
+        await user.updatePhotoURL(url);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,37 +123,133 @@ class _ProfilePageState extends State<ProfilePage> {
               const SizedBox(
                 height: 64,
               ),
-              Container(
-                padding: const EdgeInsets.all(12),
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Color(0xFFEAECF0),
+              InkWell(
+                onTap: () => showDialog(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    content: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            "Ubah Nama",
+                            style: TextStyle(
+                              color: Color(0xFF344054),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                color: const Color(0xFFD0D5DD),
+                              ),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color.fromRGBO(16, 24, 40, 0.05),
+                                  blurRadius: 2,
+                                  offset: Offset(0, 1),
+                                ),
+                              ],
+                            ),
+                            child: TextField(
+                              style: const TextStyle(fontSize: 16),
+                              controller: changeDisplayName,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Ubah Nama",
+                                contentPadding: EdgeInsets.only(
+                                    left: 14, right: 14, top: 10, bottom: 10),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Flexible(
+                            child: Align(
+                              alignment: const Alignment(0, 1),
+                              child: InkWell(
+                                onTap: () {
+                                  print("haiii ");
+                                  changeDisplayNameFirebase(
+                                      changeDisplayName.text);
+                                  Navigator.pop(context);
+                                },
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(12),
+                                    color: const Color(0xFFF5A21D),
+                                    border: Border.all(
+                                      color: const Color(0xFFF5A21D),
+                                    ),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                        color: Color.fromRGBO(16, 24, 40, 0.05),
+                                        blurRadius: 2,
+                                        offset: Offset(0, 1),
+                                      ),
+                                    ],
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 16),
+                                  child: const Text(
+                                    "Ubah",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                child: Column(
-                  children: [
-                    Row(
-                      children: const [
-                        Icon(
-                          Iconsax.edit5,
-                          color: Color.fromRGBO(219, 136, 24, 0.7),
-                        ),
-                        SizedBox(
-                          width: 16,
-                        ),
-                        Text(
-                          "Ubah profile",
-                          style: TextStyle(
-                            color: Color(0xFF344054),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Color(0xFFEAECF0),
+                      ),
                     ),
-                  ],
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(
+                            Iconsax.edit5,
+                            color: Color.fromRGBO(219, 136, 24, 0.7),
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Text(
+                            "Ubah profile",
+                            style: TextStyle(
+                              color: Color(0xFF344054),
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Container(
@@ -193,37 +318,131 @@ class _ProfilePageState extends State<ProfilePage> {
                   ],
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(12),
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Color(0xFFEAECF0),
+              InkWell(
+                onTap: () => showDialog(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    content: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            "Ubah Foto",
+                            style: TextStyle(
+                              color: Color(0xFF344054),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                color: const Color(0xFFD0D5DD),
+                              ),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color.fromRGBO(16, 24, 40, 0.05),
+                                  blurRadius: 2,
+                                  offset: Offset(0, 1),
+                                ),
+                              ],
+                            ),
+                            child: TextField(
+                              style: const TextStyle(fontSize: 16),
+                              controller: changeDisplayName,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Ubah Foto",
+                                contentPadding: EdgeInsets.only(
+                                    left: 14, right: 14, top: 10, bottom: 10),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Flexible(
+                            child: Align(
+                              alignment: const Alignment(0, 1),
+                              child: InkWell(
+                                onTap: () {
+                                  print("haiii ubah foto ");
+                                  Navigator.pop(context);
+                                },
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(12),
+                                    color: const Color(0xFFF5A21D),
+                                    border: Border.all(
+                                      color: const Color(0xFFF5A21D),
+                                    ),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                        color: Color.fromRGBO(16, 24, 40, 0.05),
+                                        blurRadius: 2,
+                                        offset: Offset(0, 1),
+                                      ),
+                                    ],
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 16),
+                                  child: const Text(
+                                    "Ubah",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                child: Column(
-                  children: [
-                    Row(
-                      children: const [
-                        Icon(
-                          Iconsax.camera5,
-                          color: Color.fromRGBO(5, 137, 214, 0.7),
-                        ),
-                        SizedBox(
-                          width: 16,
-                        ),
-                        Text(
-                          "Ubah foto",
-                          style: TextStyle(
-                            color: Color(0xFF344054),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Color(0xFFEAECF0),
+                      ),
                     ),
-                  ],
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(
+                            Iconsax.camera5,
+                            color: Color.fromRGBO(5, 137, 214, 0.7),
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Text(
+                            "Ubah foto",
+                            style: TextStyle(
+                              color: Color(0xFF344054),
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
